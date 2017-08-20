4a92ac0367cd48d90c2be876c7b92a023648b5d2
Richard Artoul <richardartoul@gmail.com>
2017-08-19 22:56:03 -0700

    Handle interrupts + read API key from file

--- hyperdash/R/hyperdash.r
+++ hyperdash/R/hyperdash.r
@@ -1,6 +1,7 @@
 library(httr)
 library(uuid)
 library(parallel)
+library(rjson)
 
 kTypeRunStarted <- 'run_started'
 kTypeRunEnded <- 'run_ended'
@@ -9,7 +10,10 @@ kTypeHeartbeat <- 'heartbeat'
 
 kOutcomeSuccess <- 'success'
 kOutcomeFailure <- 'failure'
+kOutcomeUserCanceled <- 'user_canceled'
 kLevelInfo <- 'INFO'
+
+kHyperdashJSONLocation <- "~/.hyperdash/hyperdash.json"
 
 kHeartbeatExports <- list("HeartbeatLoop", "SendSDKMessage", "POST", "CreateHeartbeatMessage", "CreateSDKMessage", "kTypeHeartbeat", "add_headers")
 
@@ -17,53 +21,69 @@ kHeartbeatExports <- list("HeartbeatLoop", "SendSDKMessage", "POST", "CreateHear
 #'
 #' This function helps you monitor a machine learning job. It will
 #' keep track of when the job starts/ends, as well as any logs that
-#' are emitted using the hd_client.print() function.
+#' are emitted using the hd.client.print() function.
 #' @param func A function that when invoked, will execute the job you want to monitor.
-#'   Note: The provided function must accept an argument called hd_client
+#'   Note: The provided function must accept an argument called hd.client
 #'   which will be passed into the function and exposes various Hyperdash
 #'   functionality. For example, if you want to print something, but also have
-#'   it available in your Hyperdash logs, you can call hd_client$print("Your log message here")
+#'   it available in your Hyperdash logs, you can call hd.client$print("Your log message here")
 #' @param job.name The name of the job that you want to monitor.
 #' @export
 #' @examples
-#' MonitorJob(function(hd_client) {
-#'   hd_client$print("Begining machine learning...")
+#' MonitorJob(function(hd.client) {
+#'   hd.client$print("Begining machine learning...")
 #'   Sys.sleep(2)
-#'   hd_client$print("25% complete...")
+#'   hd.client$print("25% complete...")
 #'   Sys.sleep(2)
-#'   hd_client$print("50% complete...")
+#'   hd.client$print("50% complete...")
 #'   Sys.sleep(2)
-#'   hd_client$print("75% complete...")
+#'   hd.client$print("75% complete...")
 #'   Sys.sleep(2)
-#'   hd_client$print("100% complete...")
+#'   hd.client$print("100% complete...")
 #'   Sys.sleep(2)
-#'   hd_client$print("Done!")
+#'   hd.client$print("Done!")
 #' }, "My test hyperdash job")
 MonitorJob <- function(func, job.name) {
+  api.key <- GetAPIKey()
+  # If we can't find an API key, just run their code
+  if (is.null(api.key)) {
+    print("Unable to locate Hyperdash API key, please make sure its located in the hyperdash.json file.")
+    # Dummy hd.client so they don't need to change their code
+    func(hd.client=NewDummyHDClient())
+  }
+
   sdk.run.uuid <- UUIDgenerate()
-  SendSDKMessage(CreateRunStartedMessage(sdk.run.uuid, job.name))
+  SendSDKMessage(CreateRunStartedMessage(sdk.run.uuid, job.name), api.key)
   # Capture result of user's function
   # Cluster of size 1 in which we will run the heartbeat code
   heartbeatCluster = makeCluster(1)
   # Since we're using the non-forking version of the makeCluster API (to support Windows) we
   # need to manually export every function that will be used by the heartbeat process.
   clusterExport(heartbeatCluster, kHeartbeatExports)
-  # Use sendCall instead of clusterCall to schedule work on the cluster without blocking
-  # the main thread of execution.
-  parallel:::sendCall(heartbeatCluster[[1]], HeartbeatLoop, list(sdk.run.uuid))
   # Capture the result of the user's code so we can return it
   outcome <- kOutcomeSuccess
   result <- tryCatch(
-    func(hd.client=NewHDClient(sdk.run.uuid)),
+    # Use sendCall instead of clusterCall to schedule work on the cluster without blocking
+    # the main thread of execution. Its important that we invoke this inside the tryCatch
+    # block so incase anything happens we're sure to cleanup the cluster in the finally
+    # call and not leave any zombie heartbeat processes around.
+    {
+      parallel:::sendCall(heartbeatCluster[[1]], HeartbeatLoop, list(sdk.run.uuid, api.key))
+      func(hd.client=NewHDClient(sdk.run.uuid, api.key))
+    },
     # Log warning
     warning = function(cond) {
-      SendSDKMessage(CreateLogMessage(sdk.run.uuid, cond$message))
+      SendSDKMessage(CreateLogMessage(sdk.run.uuid, cond$message), api.key)
       cond
     },
     # Log errors and mark job as failed
     error = function(cond) {
-      SendSDKMessage(CreateLogMessage(sdk.run.uuid, cond$message))
+      SendSDKMessage(CreateLogMessage(sdk.run.uuid, cond$message), api.key)
       outcome <<- kOutcomeFailure
+      cond
+    },
+    interrupt = function(cond) {
+      outcome <<- kOutcomeUserCanceled
       cond
     },
     # Cleanup cluster
@@ -71,32 +91,36 @@ MonitorJob <- function(func, job.name) {
       stopCluster(heartbeatCluster)
     }
   )
-  SendSDKMessage(CreateRunEndedMessage(sdk.run.uuid, outcome))    
+  SendSDKMessage(CreateRunEndedMessage(sdk.run.uuid, outcome), api.key)
   # Return result of user's function
   result
 }
 
-NewHDClient <- function(sdk.run.uuid) {
+NewHDClient <- function(sdk.run.uuid, api.key) {
   hd.print <- function(s) {
     print(s)
-    SendSDKMessage(CreateLogMessage(sdk.run.uuid, s))
+    SendSDKMessage(CreateLogMessage(sdk.run.uuid, s), api.key)
   }
   # Use a named list as an "object"
   list(print=hd.print)
 }
 
-SendSDKMessage <- function(message) {
+NewDummyHDClient <- function() {
+  list(print=print)
+}
+
+SendSDKMessage <- function(message, api.key) {
   r <- POST(
     "https://hyperdash.io/api/v1/sdk/http",
-    add_headers("x-hyperdash-auth"="<REDACTED>"),
+    add_headers("x-hyperdash-auth"=api.key),
     body=message,
     encode="json"
   )
 }
 
-HeartbeatLoop <- function(sdk.run.uuid) {
+HeartbeatLoop <- function(sdk.run.uuid, api.key) {
   repeat {
-    SendSDKMessage(CreateHeartbeatMessage(sdk.run.uuid))
+    SendSDKMessage(CreateHeartbeatMessage(sdk.run.uuid), api.key)
     Sys.sleep(10)
   }
 }
@@ -120,3 +144,29 @@ CreateLogMessage <- function(sdk.run.uuid, s) {
 CreateSDKMessage <- function(sdk.run.uuid, type, payload) {
   list(type=type, timestamp = trunc(as.numeric(Sys.time()) * 1000, prec = 0), sdk_run_uuid = sdk.run.uuid, payload = payload)
 }
+
+GetAPIKey <- function() {
+  path <- path.expand(kHyperdashJSONLocation)
+  errorMessage <- paste("Could not locate hyperdash.json file, please make sure its located at:", kHyperdashJSONLocation, "and is valid JSON", sep=" ")
+  hyperdash.json <- tryCatch(
+    fromJSON(file=path)$api_key,
+    warning = function(cond) {
+      print(errorMessage)
+      NULL    
+    },
+    error = function(cond) {
+      print(errorMessage)
+      NULL
+    }
+  )
+}
+
+MonitorJob(function(hd.client) {
+  hd.client$print("1")
+  Sys.sleep(2)
+  hd.client$print("2")
+  Sys.sleep(2)
+  hd.client$print("3")
+  Sys.sleep(300)
+  hd.client$print("Done!")
+}, "Test API key reader")
